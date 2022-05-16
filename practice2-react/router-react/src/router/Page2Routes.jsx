0718eb54abbfa6c0a0c7bcffef37c468db0cb6a7
鍋山尚希 <gachapin5689@gmail.com>
2022-05-16 17:55:52 +0900

    [update] router-react

---
+++ practice2-react/router-react/src/router/Page2Routes.jsx
@@ -0,0 +1,16 @@
+import { Page2 } from "../components/Page2";
+import { UrlParameter } from "../components/UrlParameter";
+
+
+export const Page2Routes = [
+    {
+        path: "/",
+        exact: true,
+        children: <Page2 />
+    },
+    {
+        path: "/:id",
+        exact: false,
+        children: <UrlParameter />
+    },
+]
