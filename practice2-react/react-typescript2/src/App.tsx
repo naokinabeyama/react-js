49f4727b1c7cd6f2580da025a60cbd69e63a6067
鍋山尚希 <gachapin5689@gmail.com>
2022-05-19 17:51:26 +0900

    react-typescript2

---
+++ practice2-react/react-typescript2/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Todo } from './Todo';
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+
+function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+  const onClickFetchData = () => {
+    axios.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setTodos(res.data);
+    });
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userId={todo.userId} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ practice2-react/react-typescript2/src/Todo.tsx
@@ -0,0 +1,8 @@
+
+
+export const Todo = (props) => {
+    const { title, userId } = props;
+    return (
+        <p>{`${title}(ユーザー:${userId})`}</p>
+    );
+};
