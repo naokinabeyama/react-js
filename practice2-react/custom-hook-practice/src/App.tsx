df43d9f95c670bb5f6cae54c71af8fe27e0b68ca
鍋山尚希 <gachapin5689@gmail.com>
2022-05-25 16:15:31 +0900

    custom-hook-practice

---
+++ practice2-react/custom-hook-practice/src/App.tsx
@@ -0,0 +1,29 @@
+import { UserCard } from './components/UserCard';
+import { useAllUsers } from './hooks/useAllUsers';
+
+
+function App() {
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+
+  const onClickFetchUser = () => getUsers();
+
+  return (
+    <div className="App">
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
