3ae0249e67055bf55685b3be282adc753317bd43
鍋山尚希 <gachapin5689@gmail.com>
2022-05-26 18:01:48 +0900

    [update] summary-react

--- practice2-react/summary-react/src/App.tsx
+++ practice2-react/summary-react/src/App.tsx
@@ -1,10 +1,17 @@
-import './App.css';
-import { ChakraProvider, Button } from '@chakra-ui/react';
+import { theme } from './theme/theme';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './router/Router';
+
+
 
 function App() {
   return (
-    <ChakraProvider>
-      <Button colorScheme="teal">ボタン</Button>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
---
+++ practice2-react/summary-react/src/router/Router.tsx
@@ -0,0 +1,30 @@
+import { memo, VFC } from 'react'
+import { Route, Switch } from 'react-router-dom';
+import { Login } from '../components/pages/Login';
+import { Page404 } from '../components/pages/Page404';
+import { HeaderLayout } from '../components/templates/HeaderLayout';
+import { HomeRouters } from './HomeRouters';
+
+export const Router: VFC = memo(() => {
+    return (
+        <Switch>
+            <Route exact path="/">
+                <Login />
+            </Route>
+            <Route path="/home" render={({ match: { url } }) => (
+                <Switch>
+                    {HomeRouters.map((route) => (
+                        <Route key={route.path} exact={route.exact} path={`${url}${route.path}`}>
+                            <HeaderLayout>
+                                {route.children}
+                            </HeaderLayout>
+                        </Route>
+                    ))}
+                </Switch>
+            )} />
+            <Route path="*">
+                <Page404 />
+            </Route>
+        </Switch>
+    )
+});
