58a116f53928c92969ff2877f989456be8c3af3e
鍋山尚希 <gachapin5689@gmail.com>
2022-05-27 18:13:31 +0900

    [update] summary-react

--- practice2-react/summary-react/src/components/pages/UserManagement.tsx
+++ practice2-react/summary-react/src/components/pages/UserManagement.tsx
@@ -1,7 +1,32 @@
-import { memo, VFC } from "react"
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react"
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
+    const { getUsers, users, loading } = useAllUsers();
+
+    useEffect(() => getUsers(), []);
+
     return (
-        <p>ユーザー管理ページです</p>
+        <>
+            {loading ? (
+                <Center h="100vh">
+                    <Spinner />
+                </Center>
+            ) : (
+                <Wrap p={{ base: 4, md: 10 }} justify='space-around'>
+                    {users.map((user) => (
+                        <WrapItem key={user.id} >
+                            <UserCard
+                                imageUrl="https://source.unsplash.com/random"
+                                userName={user.username}
+                                fullName={user.name} />
+                        </WrapItem>
+                    ))}
+                </Wrap>
+            )}
+        </>
     )
 });
